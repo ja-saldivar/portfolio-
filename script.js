1a10d73c49ad948e45645319f6715099b714bdb6
Antonio Saldivar <jediknight_01@hotmail.com>
2022-02-21 14:40:05 -0600

    Finished html, css ,js

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-let styleProject = document.getElementById('webstyle');
-let excursionProject = document.getElementById('excursion');
-let teaProject = document.getElementById('tea');
+var menuItem = document.querySelectorAll('.menu-item');
 
-function zoom(event) {
-  event.target.style.transform = 'scale(2)';
+for (let i = 0; i < menuItem.length; i++) {
+  let item = menuItem[i];
+  item.addEventListener('click', function (ev) {
+    ev.preventDefault();
+    const sections = document.querySelectorAll('.section');
+    //console.log(this.dataset.menu);
+    for (let j = 0; j < sections.length; j++) {
+      const section = sections[j];
+      section.style.display = 'none';
+    };
+    const currentSection = document.getElementById(this.dataset.menu + '-tab');
+    currentSection.style.display = 'block';
+  });
 }
 
-styleProject.addEventListener('mouseover', zoom);
-excursionProject.addEventListener('mouseover', zoom);
-teaProject.addEventListener('mouseover', zoom);
+menuItem[0].addEventListener('click', function() {
+  menuItem[1].style.borderBottom = 'none';
+  menuItem[2].style.borderBottom = 'none';
+})
+
+menuItem[1].addEventListener('click', function(ev) {
+  menuItem[2].style.borderBottom = 'none';
+  ev.target.style.borderBottom = '0.15rem solid #000';
+})
+
+menuItem[2].addEventListener('click', function(ev) {
+  menuItem[1].style.borderBottom = 'none';
+  ev.target.style.borderBottom = '0.15rem solid #000';
+})
